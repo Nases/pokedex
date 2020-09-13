2c36c5bc392cdd59514cd572bfd279a095356afd
Hasan Sefa Ozalp <hasan@hasansefaozalp.com>
2020-09-13 01:27:47 -0700

    api main logic

--- client/components/Pokemon.js
+++ client/components/Pokemon.js
@@ -2,14 +2,71 @@ import Grid from '@material-ui/core/Grid'
 import axios from 'axios'
 
 
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 345,
+  },
+  media: {
+    height: 140,
+  },
+});
+
 const Pokemon = ({ url, name }) => {
+  const classes = useStyles();
+
+  var img
   axios.get(url).then(value => {
-    console.log(value.data)
+    var data = value.data
+    console.log(data.sprites.other.dream_world.front_default)
+    img = data.sprites.other.dream_world.front_default
   })
+
+  // var img = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg'
+
 
   return (
     <Grid item xs={3}>
-      {name}
+
+
+      <Card className={classes.root}>
+        <CardActionArea>
+          <CardMedia
+            className={classes.media}
+            // image="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
+            image={img}
+            title="Contemplative Reptile"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="h2" align='center'>
+              {name}
+            </Typography>
+            <Typography variant="body2" color="textSecondary" component="p">
+              Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+              across all continents except Antarctica
+          </Typography>
+          </CardContent>
+        </CardActionArea>
+        <CardActions>
+          <Button size="small" color="primary">
+            Share
+        </Button>
+          <Button size="small" color="primary">
+            Learn More
+        </Button>
+        </CardActions>
+      </Card>
+
+
     </Grid>
 
   )

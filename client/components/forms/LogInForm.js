607597172be755d8a7ad64db7ecc2c54e18682b4
Hasan Sefa Ozalp <hasan@hasansefaozalp.com>
2020-09-14 20:46:54 -0700

    login integration completed

--- client/components/forms/LogInForm.js
+++ client/components/forms/LogInForm.js
@@ -1,19 +1,16 @@
-import React from 'react'
+import { Formik, Form, Field, ErrorMessage } from 'formik'
+import FormErrorMessage from './partials/FormErrorMessage'
+import { LoginSchema } from '../../assets/validation/schemas'
+import userUtils from '../../assets/userUtils'
+import { useUser, useDispatchUser } from '../../contexts/UserProvider/UserProvider'
+import Router from 'next/router'
 import Avatar from '@material-ui/core/Avatar'
 import Button from '@material-ui/core/Button'
-import CssBaseline from '@material-ui/core/CssBaseline'
 import TextField from '@material-ui/core/TextField'
-import FormControlLabel from '@material-ui/core/FormControlLabel'
-import Checkbox from '@material-ui/core/Checkbox'
 import Link from 'next/link'
-import Grid from '@material-ui/core/Grid'
-import Box from '@material-ui/core/Box'
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined'
 import Typography from '@material-ui/core/Typography'
 import { makeStyles } from '@material-ui/core/styles'
 import Container from '@material-ui/core/Container'
-import { useUser, useDispatchUser } from '../../contexts/UserProvider/UserProvider'
-
 
 
 const useStyles = makeStyles((theme) => ({
@@ -42,7 +39,7 @@ const useStyles = makeStyles((theme) => ({
   }
 }))
 
-export default function SignUp() {
+const LogInForm = () => {
   const classes = useStyles()
   const dispatchUserData = useDispatchUser()
   const user = useUser()
@@ -51,59 +48,76 @@ export default function SignUp() {
 
   return (
     <Container component="main" maxWidth="xs" className='mb-10'>
-      <CssBaseline />
       <div className={classes.paper}>
         <Avatar className={classes.avatar}>
-          <i aria-hidden className="fas fa-user-plus"></i>
+          <i aria-hidden className="fas fa-sign-in-alt"></i>
         </Avatar>
         <Typography component="h1" variant="h5">
           Log In
         </Typography>
-        <form className={classes.form} noValidate>
-          <Grid container spacing={2}>
-            <Grid item xs={12}>
-              <TextField
-                variant="outlined"
-                required
+        <Formik
+          initialValues={{
+            email: '',
+            password: '',
+            serverError: ''
+          }}
+          validateOnBlur={false}
+          validateOnChange={false}
+          validationSchema={LoginSchema}
+          onSubmit={(values, { setSubmitting, setFieldError }) => {
+            userUtils.login(values.email, values.password)
+              .then(response => {
+                dispatchUserData({
+                  type: 'LOGIN',
+                  userData: response.data
+                })
+                Router.push('/pokemons')
+                // console.log(response)
+                // setSubmitting(false)
+              })
+              .catch((error) => {
+                // console.log(error)
+                dispatchUserData({
+                  type: 'SET_IS_LOADING_FALSE'
+                })
+                setFieldError('serverError', error.response.data)
+                setSubmitting(false)
+              })
+          }}
+        >
+          {({ isSubmitting, values }) => (
+            <Form className={classes.form}>
+              <ErrorMessage name="serverError" component={FormErrorMessage} />
+              <div className='mb-4'>
+                <Field id='email' type="email" name="email" placeholder='you@example.com' variant="outlined" fullWidth label="Email address" autoComplete="email" autoFocus as={TextField} />
+                <ErrorMessage name="email" component={FormErrorMessage} />
+              </div>
+              <div className='mb-4'>
+                <Field id='password' type="password" name="password" placeholder='&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;' variant="outlined" fullWidth label="Password" as={TextField} />
+                <ErrorMessage name="password" component={FormErrorMessage} />
+              </div>
+              <Button
+                type="submit"
+                disabled={isSubmitting}
+                type="submit"
                 fullWidth
-                id="email"
-                label="Email Address"
-                name="email"
-                autoComplete="email"
-                autoFocus
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                variant="outlined"
-                required
-                fullWidth
-                name="password"
-                label="Password"
-                type="password"
-                id="password"
-                autoComplete="current-password"
-              />
-            </Grid>
-          </Grid>
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            color="primary"
-            className={classes.submit}
-          >
-            Sign Up
-          </Button>
-          <Grid container justify="flex-end">
-            <Grid item>
-              <Link href="/login" >
-                <a className={classes.loginLink}>Already have an account? Log in</a>
-              </Link>
-            </Grid>
-          </Grid>
-        </form>
+                variant="contained"
+                color="primary"
+              >
+                Log In
+            </Button>
+            </Form>
+          )}
+        </Formik>
+        <div className='mt-3 w-full text-right'>
+          <Link href="/signup">
+            <a className={classes.loginLink}>Don't have an account? Sign up</a>
+          </Link>
+        </div>
       </div>
     </Container>
   )
 }
+
+
+export default LogInForm

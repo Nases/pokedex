c691bc0bcaf6a52a84ee3795ff916aee1f3e4f5e
Hasan Sefa Ozalp <hasan@hasansefaozalp.com>
2020-09-14 20:20:19 -0700

    logout functionality added

---
+++ client/pages/logout.js
@@ -0,0 +1,29 @@
+import { useEffect } from 'react'
+import { useUser, useDispatchUser } from '../contexts/UserProvider/UserProvider'
+import userUtils from '../assets/userUtils'
+import Router from 'next/router'
+
+
+const LogOut = () => {
+  const userDataDispatch = useDispatchUser()
+
+  const signOut = async () => {
+    await userUtils.signOut()
+      .then(response => {
+        // console.log(response)
+        userDataDispatch({ type: 'SIGN_OUT' })
+      })
+      .catch(error => {
+        // console.log(error)
+        userDataDispatch({ type: 'SIGN_OUT' })
+      })
+  }
+
+  useEffect(() => {
+    signOut()
+    Router.push('/')
+  }, [])
+  return ('')
+}
+
+export default LogOut

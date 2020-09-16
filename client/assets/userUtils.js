9218ab57dfeaa9e315dc960f33dfdaeb768122f4
Hasan Sefa Ozalp <hasan@hasansefaozalp.com>
2020-09-16 01:29:44 -0700

    clg serveruri

--- client/assets/userUtils.js
+++ client/assets/userUtils.js
@@ -3,6 +3,7 @@ import axios from 'axios'
 
 
 const serverURI = (process.env.NODE_ENV === 'production') ? 'http://159.89.135.165:5000/' : 'http://localhost:5000/'
+console.log(serverURI)
 
 const userUtils = {
   login: (email, password) => {
